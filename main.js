8ef4273d0ccff0202f9390e4d8675cb20df77ae5
Jackfortal <adaojacksud@gmail.com>
2024-02-11 10:47:28 -0300

    conclusão do header menu, inicio da segunda etapa

---
+++ main.js
@@ -0,0 +1,8 @@
+function playGif() {
+    document.getElementById('gif').classList.add('animated-gif');
+  }
+  
+  function pauseGif() {
+    document.getElementById('gif').classList.remove('animated-gif');
+  }
+  
