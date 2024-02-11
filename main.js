ad9375d4c01b4be2b0a97fd523973a79f800c4f1
Jackfortal <adaojacksud@gmail.com>
2024-02-11 13:57:26 -0300

    avaliação e ajustes no head

--- main.js
+++
@@ -1,8 +0,0 @@
-function playGif() {
-    document.getElementById('gif').classList.add('animated-gif');
-  }
-  
-  function pauseGif() {
-    document.getElementById('gif').classList.remove('animated-gif');
-  }
-  
